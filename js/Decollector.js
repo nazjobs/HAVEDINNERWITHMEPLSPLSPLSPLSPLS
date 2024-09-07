76e73de3bee85fbe64c80d1f069adb0c2bf6a65c
Nazrawi <nazrawiijobs@gmail.com>
2024-09-07 21:52:34 +0300

    Revert "no cors test"
    This reverts commit 5f6b937104409d58aa2ebc21d3c64aaa376766ce.

--- js/Decollector.js
+++ js/Decollector.js
@@ -53,8 +53,12 @@ function collectPageData() {
 
 function sendDataToGoogleSheet(data) {
    // Send the foods no fucking errors... yeh? no? no? yeh? okay. 
+<<<<<<< HEAD
   fetch('https://script.google.com/macros/s/AKfycbzK5ohijBLp1QqUw1GNq7DtzD2gHg7W0M1s3HlvHN6qv14L7A42fqMDYBHhDrPO8oNa/exec', {
     mode: 'no-cors',
+=======
+  fetch('https://script.google.com/macros/s/AKfycbwLxgFt87T4zYa8zcueb7QPOyzKAYN4jUBJZ_6I3UdtmaNBfDal8_2qeD8X4mkVgZKQ/exec', {
+>>>>>>> parent of 5f6b937 (no cors test)
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
